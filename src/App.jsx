0b522297e94edf44a8647efcea60057875cbd648
MarynaVCH <83900520+MarynaVCH@users.noreply.github.com>
2022-02-23 00:02:24 +0200

    add code split

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
+import { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
-import HomePage from './pages/HomePage';
-import MoviesPage from './pages/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
+
+const HomePage = lazy(() =>
+  import('./pages/HomePage' /* webpackChunkName: 'homePage' */),
+);
+const MoviesPage = lazy(() =>
+  import('./pages/MoviesPage' /* webpackChunkName: 'moviesPage' */),
+);
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage' /* webpackChunkName: 'movieDetailsPage' */),
+);
 
 export default function App() {
   return (
     <Container>
       <AppBar />
 
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
 
-        <Route path="/movies" exact>
-          <MoviesPage />
-        </Route>
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
 
-        <Route path="/movies/:moviesId">
-          <MovieDetailsPage />
-        </Route>
-      </Switch>
+          <Route path="/movies/:moviesId">
+            <MovieDetailsPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </Container>
   );
 }
